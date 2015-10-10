4006e44f36778edd9ba9b0d9710a03124b23863a
joshram27 <Jo304520074>
2015-10-10 16:26:25 -0600

    Respaldo de la clase 10/10, semana 5

---
+++ DemoAccesoBaseDatos/AdventureWorks2014.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks2014.Model
+{
+    public class Class1
+    {
+    }
+}
